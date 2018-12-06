4bc4ee374199be773628a6235b190e42f59fdba8
aehomay <a.e.homay@gmail.com>
2018-12-06 22:02:07 +0100

    The first clean commit

---
+++ NancyFxMicroservice/HelloWorldModule.cs
@@ -0,0 +1,21 @@
+﻿using Nancy;
+using Nancy.Extensions;
+using System;
+
+namespace NancyFxMicroservice
+{
+	public class HelloWorldModule : NancyModule
+	{
+		public HelloWorldModule():base("api")
+		{
+			Get["/values"] = parameters => new string[] { "Hello", "World", "...!" };
+			Get["/values/{id}"] = parameters => (parameters.id.Value == "1") ? "Hello" : (parameters.id.Value == "2") ? "World" : (parameters.id.Value == "3") ? "...!" : "No world found... ;-)";
+			Post["/values"] = _ => 
+			{
+				var value = Context.Request.Body.AsString();
+				Console.WriteLine($"The received value is:{value}");
+				return HttpStatusCode.OK;
+			};
+		}
+	}
+}
---
+++ NancyFxMicroservice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace NancyFxMicroservice
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var host = new NancyHost(new Uri("http://localhost:9000")))
+			{
+				host.Start();
+				Console.WriteLine("Running on http://localhost:9000");
+				Console.ReadLine();
+			}
+		}
+	}
+}
